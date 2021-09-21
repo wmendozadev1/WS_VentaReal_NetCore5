06dfe74732c95188a87b48203ac4cd745e12bde6
Winston Mendoza G <wjmendozag87@gmail.com>
2021-09-21 12:09:37 -0600

    configurando cors

--- Startup.cs
+++ Startup.cs
@@ -16,6 +16,7 @@ namespace WS_VentaReal_NetCore5
 {
     public class Startup
     {
+        readonly string MiCors = "MiCors";
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -26,6 +27,14 @@ namespace WS_VentaReal_NetCore5
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options => {
+                options.AddPolicy(name: MiCors,
+                                    builder =>
+                                    {
+                                        builder.WithOrigins("*");
+                                    });
+            
+            });
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -47,6 +56,8 @@ namespace WS_VentaReal_NetCore5
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(MiCors);
 
             app.UseAuthorization();
 
