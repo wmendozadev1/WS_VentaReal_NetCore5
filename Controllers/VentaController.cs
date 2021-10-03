6ba254bb69100375fafba1db1b14545b3e4cae49
Winston Mendoza G <wjmendozag87@gmail.com>
2021-10-03 10:53:39 -0600

    Crecion de post para el ventaController, usando transacciones EF

---
+++ Controllers/VentaController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WS_VentaReal_NetCore5.Models;
+using WS_VentaReal_NetCore5.Models.Request;
+using WS_VentaReal_NetCore5.Models.Response;
+
+namespace WS_VentaReal_NetCore5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class VentaController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Add(VentaRequest model)
+        {
+            Respuesta respuesta = new Respuesta();
+
+            try
+            {
+                using (VentaRealContext db = new VentaRealContext())
+                {
+                    using (var transaction = db.Database.BeginTransaction())
+                    {
+                        try
+                        {
+                            var venta = new Venta();
+                            venta.Total = model.Conceptos.Sum(d => d.Cantidad * d.PrecioUnitario); // model.Total;
+                            venta.Fecha = DateTime.Now;
+                            venta.IdCliente = model.IdCliente;
+                            db.Ventas.Add(venta);
+                            db.SaveChangesAsync();
+
+                            foreach (var modelconcepto in model.Conceptos)
+                            {
+                                var concepto = new Models.Concepto();
+                                concepto.Cantidad = modelconcepto.Cantidad;
+                                concepto.IdProducto = modelconcepto.IdProducto;
+                                concepto.PrecioUnitario = modelconcepto.PrecioUnitario;
+                                concepto.Importe = modelconcepto.Importe;
+                                concepto.IdVenta = venta.Id;
+                                db.Conceptos.Add(concepto);
+                                db.SaveChanges();
+                            }
+
+                            transaction.Commit();
+                            respuesta.Exito = 1;
+                        }
+                        catch (Exception )
+                        {
+                            transaction.Rollback();
+                        }
+                    }
+                  
+                }
+
+            }
+            catch(Exception ex)
+            {
+                respuesta.Mensaje = ex.Message;
+
+            }
+
+            return Ok(respuesta);
+        }
+    }
+}
---
+++ Models/Request/VentaRequest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WS_VentaReal_NetCore5.Models.Request
+{
+    public class VentaRequest
+    {
+        public int IdCliente { get; set; }
+        public decimal Total { get; set; }
+
+        public List<Concepto> Conceptos { get; set; }
+
+        public VentaRequest()
+        {
+            this.Conceptos = new List<Concepto>();
+        }
+
+    }
+
+    public class Concepto
+    {
+        public int Cantidad { get; set; }
+        public decimal PrecioUnitario { get; set; }
+        public decimal Importe { get; set; }
+        public int IdProducto { get; set; }
+
+    }
+}
